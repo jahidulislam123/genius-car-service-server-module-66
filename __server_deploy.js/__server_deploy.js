bb8ca892e299940bd8b73380e4ee25a9c881a203
unknown <islam15-3879@diu.edu.bd>
2022-05-01 16:33:25 +0600

    adding-steps

---
+++ __server_deploy.js/__server_deploy.js
@@ -0,0 +1,16 @@
+/**
+ * 
+ * -------------------
+ * one time
+ * 1: crete heroku account 
+ * 2: veryfy email
+ * 3: install heruku cli 
+ * 4: login
+ * 
+ * 
+ * --------------------
+ * 
+ * for each projects one time 
+ * 1: heroku create 
+ * 2: 
+ */
