8dde9a632666521eb74d89ad183e259cba046d87
unknown <islam15-3879@diu.edu.bd>
2022-05-01 15:28:19 +0600

    jwt-token

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const cors = require('cors');
+const jwt=require('jsonwebtoken');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const jsonwebtoken = require('jsonwebtoken');
 
 const app =express();
 const port =process.env.PORT ||5000;
@@ -12,6 +14,25 @@ app.use(express.json());
 
 
 //eta mongodb er connect theke copy kora code 
+
+function verifyJWT(req,res,next){
+    const authHeader =req.headers.authorization;
+
+if(!authHeader){
+    return res.status(401).send({message:'unauthorized access'});
+}
+const token =authHeader.split(' ')[1];
+jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,decoded)=>{
+    if(err){
+        return res.status(403).send({message:'Forbidden access'});
+    }
+    console.log('decoded',decoded);
+    req.decoded=decoded;
+})
+        //    console.log('inside verify JWT ',authHeader);
+           next();
+}
+
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.c6wxw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -28,6 +49,17 @@ async function run(){
         const services =await cursor.toArray();
         res.send(services);
         });
+
+        //auth
+        app.post('/login',async(req,res)=>{
+            const user =req.body;
+            const accessToken =jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{
+                expiresIn:'1d'
+            });
+            res.send(accessToken);
+        })
+
+        // services api 
         app.get('/service/:id',async(req,res)=>{
             const id =req.params.id;
             const query ={_id:ObjectId(id)};
@@ -47,6 +79,22 @@ async function run(){
            const query ={_id:ObjectId(id)}
            const result =await serviceCollection.deleteOne(query);
            res.send(result);
+       })
+
+       app.get('/order', verifyJWT, async(req,res)=>{
+           
+        const decodedEmail =req.decoded.email;
+           const email =req.query.email;
+        //    console.log(email);
+           if(email===decodedEmail){
+            const query ={email:email};
+            const cursor =orderCollection.find(query);
+            const orders = await cursor.toArray();
+            res.send(orders);
+           }
+           else{
+               res.status(403).send({message:'Forbidden acces '})
+           }
        })
 
        //order collection api
